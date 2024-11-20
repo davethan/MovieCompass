4a87f5abee8052072413c61d9ff31b51a43d41a9
Danis Desipris <danis_desipris@hotmail.com>
2024-11-21 00:05:46 +0200

    Added scss, drawer

---
+++ frontend/src/components/TheDrawer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="offcanvas offcanvas-start bg-dark text-light" tabindex="-1" id="offcanvasExample"
+    aria-labelledby="offcanvasExampleLabel">
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+      <button type="button" class="btn-close btn-close-white text-reset" data-bs-dismiss="offcanvas"
+        aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+      <div class="list-group">
+        <div class="list-group-item bg-dark text-light">
+          Search
+        </div>
+        <div class="list-group-item bg-dark text-light">
+          Sort by
+        </div>
+        <div class="list-group-item bg-dark text-light">
+          Choose by day
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
--- frontend/src/components/TheHeader.vue
+++ frontend/src/components/TheHeader.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="navbar sticky-top navbar-dark bg-dark p-3">
-    Movie compass
+  <div class="navbar sticky-top navbar-dark bg-dark px-3 shadow-lg">
+    <div class="d-flex gap-3 align-items-center">
+      <i class="bi bi-list fs-1" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" />
+      Movie Compass
+    </div>
   </div>
+  <TheDrawer />
 </template>
 
 <script setup>
+import TheDrawer from './TheDrawer.vue';
 
 </script>
