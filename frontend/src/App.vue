a26fc63dbec23bee0f8bacd0f197ad08681763ea
Danis Desipris <danis_desipris@hotmail.com>
2024-11-21 09:54:58 +0200

    Added filters and sortings

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,24 @@
 <template>
-  <TheHeader />
-  <!-- <RouterLink to="/">Home</RouterLink> -->
-  <div class="appBody">
-    <RouterView />
+  <LoadingView v-if="moviesStore.loading" />
+  <div v-else>
+    <TheHeader />
+    <!-- <RouterLink to="/">Home</RouterLink> -->
+    <div class="appBody">
+      <RouterView />
+    </div>
   </div>
 </template>
 
 <script setup>
-import { /*RouterLink,*/ RouterView } from 'vue-router'
-import TheHeader from '@/components/TheHeader.vue'
+import { onMounted } from 'vue';
+import { /*RouterLink,*/ RouterView } from 'vue-router';
+import TheHeader from '@/components/TheHeader.vue';
+import LoadingView from '@/views/LoadingView.vue';
+import { useMoviesStore } from './stores/movies';
+
+const moviesStore = useMoviesStore();
+
+onMounted(async () => {
+  // await moviesStore.getAllCurrentMoviesDetails();
+})
 </script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <template>
-  <div v-if="loading">
-    <div class="spinner-border" role="status" />
-  </div>
-  <div v-else class="d-flex flex-column gap-3">
-    <div class="card bg-dark text-light" v-for="movie in sortedMovies" :key="movie.id">
+  <div class="d-flex flex-column gap-3">
+    <div class="d-flex gap-2 align-items-center">
+      <button :class="`btn btn-outline-light ${sortedBy === 1 ? 'text-primary' : ''}`"
+        @click="sortByPopularity()">Popularity</button>
+      <button :class="`btn btn-outline-light ${sortedBy === 2 ? 'text-primary' : ''}`"
+        @click="sortByRating()">Rating</button>
+      <button class="btn btn-outline-light" @click="filterByToday()">Today</button>
+      {{ state.length }}/{{ moviesStore.MOVIES.length }}
+    </div>
+    <div class="card bg-dark text-light" v-for="movie in state" :key="movie.id">
       <div class="card-header">
         <h1>{{ movie.greekTitle }}</h1>
         <div class="d-flex justify-content-between flex-wrap column-gap-3">
@@ -40,12 +45,12 @@
 </template>
 
 <script setup>
-import { onMounted, ref, computed } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useMoviesStore } from '@/stores/movies';
 
 const moviesStore = useMoviesStore();
 const state = ref(moviesStore.MOVIES);
-const loading = ref(false);
+const sortedBy = ref(1)
 
 const formatDuration = (minutes) => {
   const hours = Math.floor(minutes / 60);
@@ -60,8 +65,9 @@ const formatDuration = (minutes) => {
   }
 };
 
-const sortedMovies = computed(() => {
-  return [...state.value].sort((a, b) => {
+const sortByPopularity = () => {
+  sortedBy.value = 1;
+  state.value = moviesStore.MOVIES.sort((a, b) => {
     if (a.cinemas.length !== b.cinemas.length) {
       return b.cinemas.length - a.cinemas.length;
     }
@@ -76,11 +82,46 @@ const sortedMovies = computed(() => {
     }
     return 0;
   });
-});
+};
 
-onMounted(async () => {
-  loading.value = true;
-  //await moviesStore.getAllCurrentMoviesDetails();
-  loading.value = false;
-})
+const sortByRating = () => {
+  sortedBy.value = 2;
+  state.value = moviesStore.MOVIES.sort((a, b) => {
+    if (a.imdbRating && b.imdbRating) {
+      if (b.imdbRating !== a.imdbRating) {
+        return b.imdbRating - a.imdbRating;
+      }
+    }
+    if (!a.imdbRating && b.imdbRating) {
+      return 1;
+    }
+    if (a.imdbRating && !b.imdbRating) {
+      return -1;
+    }
+    return b.cinemas.length - a.cinemas.length;
+  });
+};
+
+const filterByToday = () => {
+  const date = new Date();
+  const today = date.toLocaleDateString('en-US', { weekday: 'long' });
+  state.value = moviesStore.MOVIES.filter((film) => {
+    return film.cinemas.some((cinema) => {
+      return cinema.cinemaSchedule[today] && cinema.cinemaSchedule[today].length > 0;
+    });
+  });
+};
+
+const filterBySelectedDays = (days) => {
+  days = ['Wednesday', 'Monday']
+  state.value = moviesStore.MOVIES.filter((film) => {
+    return film.cinemas.some((cinema) => {
+      return days.some((day) => {
+        return cinema.cinemaSchedule[day] && cinema.cinemaSchedule[day].length > 0;
+      })
+    });
+  });
+};
+
+onMounted(() => sortByPopularity());
 </script>
