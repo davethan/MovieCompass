aaad8657f1618e6e3840f20b8ff51f80f7a3b297
Danis Desipris <danis_desipris@hotmail.com>
2024-11-23 22:16:05 +0200

    Better component data sharing

--- frontend/src/components/TheDrawer.vue
+++ frontend/src/components/TheDrawer.vue
@@ -2,18 +2,50 @@
   <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
     <div class="offcanvas-header">
       <button type="button" class="mx-0 py-0 btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      <h5 class="ms-auto offcanvas-title" id="offcanvasExampleLabel">MovieCompass</h5>
+      <h5 class="ms-auto offcanvas-title" id="offcanvasExampleLabel">Εργαλεία</h5>
     </div>
     <div class="offcanvas-body">
-      <div class="list-group">
-        <div class="list-group-item">
-          Search
+      <div class="row g-2">
+        <div class="col-6">
+          <button :class="`btn w-100 ${sortedBy === POPULARITY ? 'btn-primary' : 'btn-outline-primary'}`"
+            @click="sortByPopularity()">
+            <i class="bi bi-fire me-1"></i>Δημοφιλία
+          </button>
         </div>
-        <div class="list-group-item">
-          Sort by
+        <div class="col-6">
+          <button v-if="!moviesStore.loadingRating"
+            :class="`btn w-100 ${sortedBy === RATING ? 'btn-primary' : 'btn-outline-primary'}`"
+            @click="sortByRating()"><i class="bi bi-star-half me-1"></i>Βαθμολογία</button>
+          <div v-else class="placeholder-glow h-100 w-100">
+            <span class="placeholder h-100 rounded-2 col-12"></span>
+          </div>
         </div>
-        <div class="list-group-item">
-          Choose by day
+        <div class="col-4">
+          <button :class="`btn w-100 ${filteredByDay === EVERY_DAY ? 'btn-primary' : 'btn-outline-primary'}`"
+            @click="resetDayFilter()">Κάθε μέρα</button>
+        </div>
+        <div class="col-4">
+          <button :class="`btn w-100 ${filteredByDay === TODAY ? 'btn-primary' : 'btn-outline-primary'}`"
+            @click="filterByToday()">Σήμερα</button>
+        </div>
+        <div class="col-4">
+          <button :class="`btn w-100 ${filteredByDay === WEEKEND ? 'btn-primary' : 'btn-outline-primary'}`"
+            @click="filterByWeekend()">Το ΣΚ</button>
+        </div>
+        <div class="col-4">
+          <button :class="`btn w-100 ${filteredByCinema === ALL_CINEMAS ? 'btn-primary' : 'btn-outline-primary'}`"
+            @click="resetCinemaFilter()">Όλοι</button>
+        </div>
+        <div class="col-4">
+          <button :class="`btn w-100 ${filteredByCinema === SUMMER_CINEMAS ? 'btn-primary' : 'btn-outline-primary'}`"
+            @click="filterBySummerCinemas()">Θερινοί</button>
+        </div>
+        <div class="col-4">
+          <button :class="`btn w-100 ${filteredByCinema === WINTER_CINEMAS ? 'btn-primary' : 'btn-outline-primary'}`"
+            @click="filterByWinterCinemas()">Χειμερινοί</button>
+        </div>
+        <div class="col-12">
+          <button class="btn w-100 btn-outline-primary">Περιοχές</button>
         </div>
       </div>
     </div>
@@ -21,4 +53,104 @@
 </template>
 
 <script setup>
+import { ref, inject } from 'vue';
+import { useMoviesStore } from '@/stores/movies';
+
+const moviesStore = useMoviesStore();
+const state = inject("state");
+
+const EVERY_DAY = 1, TODAY = 2, WEEKEND = 3;
+const ALL_CINEMAS = 1, SUMMER_CINEMAS = 2, WINTER_CINEMAS = 3;
+const POPULARITY = 1, RATING = 2;
+
+const sortedBy = ref(POPULARITY);
+const filteredByDay = ref(EVERY_DAY);
+const filteredByCinema = ref(ALL_CINEMAS);
+
+const sortByPopularity = () => {
+  sortedBy.value = POPULARITY;
+  state.value = state.value.sort((a, b) => {
+    if (a.cinemas.length !== b.cinemas.length) {
+      return b.cinemas.length - a.cinemas.length;
+    }
+    if (a.imdbRating && b.imdbRating) {
+      return b.imdbRating - a.imdbRating;
+    }
+    if (!a.imdbRating && b.imdbRating) {
+      return 1;
+    }
+    if (a.imdbRating && !b.imdbRating) {
+      return -1;
+    }
+    return 0;
+  });
+};
+
+const sortByRating = () => {
+  sortedBy.value = RATING;
+  state.value = state.value.sort((a, b) => {
+    if (a.imdbRating && b.imdbRating) {
+      if (b.imdbRating !== a.imdbRating) {
+        return b.imdbRating - a.imdbRating;
+      }
+    }
+    if (!a.imdbRating && b.imdbRating) {
+      return 1;
+    }
+    if (a.imdbRating && !b.imdbRating) {
+      return -1;
+    }
+    return b.cinemas.length - a.cinemas.length;
+  });
+};
+
+const filterByToday = () => {
+  filteredByDay.value = TODAY;
+  const date = new Date();
+  const today = date.toLocaleDateString('en-US', { weekday: 'long' });
+  state.value = state.value.filter((film) => {
+    return film.cinemas.some((cinema) => {
+      return cinema.cinemaSchedule[today] && cinema.cinemaSchedule[today].length > 0;
+    });
+  });
+};
+
+const resetDayFilter = () => {
+  filteredByDay.value = EVERY_DAY;
+  state.value = moviesStore.MOVIES;
+};
+const filterByWeekend = (days) => {
+  filteredByDay.value = WEEKEND;
+  days = ['Saturday', 'Sunday'];
+  state.value = state.value.filter((film) => {
+    return film.cinemas.some((cinema) => {
+      return days.some((day) => {
+        return cinema.cinemaSchedule[day] && cinema.cinemaSchedule[day].length > 0;
+      })
+    });
+  });
+};
+
+const resetCinemaFilter = () => {
+  filteredByCinema.value = 1;
+};
+const filterBySummerCinemas = () => {
+  filteredByCinema.value = 2;
+};
+const filterByWinterCinemas = () => {
+  filteredByCinema.value = 3;
+};
+
+/*
+const filterBySelectedLocations = (locations) => {
+  locations = ['Wednesday', 'Monday']
+  state.value = state.value.filter((film) => {
+    return film.cinemas.some((cinema) => {
+      return locations.some((day) => {
+        return cinema.cinemaSchedule[day] && cinema.cinemaSchedule[day].length > 0;
+      })
+    });
+  });
+};
+*/
 </script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,28 +1,17 @@
 <template>
   <div class="d-flex flex-column gap-3">
-    <div class="d-flex gap-2 align-items-center flex-wrap">
-      <button :class="`btn btn-outline-light ${sortedBy === 1 ? 'text-primary' : ''}`"
-        @click="sortByPopularity()">Popularity</button>
-      <button v-if="!moviesStore.loadingRating" :class="`btn btn-outline-light ${sortedBy === 2 ? 'text-primary' : ''}`"
-        @click="sortByRating()">Rating</button>
-      <div v-else class="btn btn-outline-light placeholder-glow" disabled>
-        <span class="placeholder col-6"></span>
-      </div>
-      <button class="btn btn-outline-light" @click="filterByToday()">Today</button>
-      <button class="btn btn-outline-light" @click="resetDayFilter()">Reset Today</button>
-      <button class="btn btn-outline-light" @click="resetDayFilter()">Filter by location</button>
-      {{ state.length }}/{{ moviesStore.MOVIES.length }}
-    </div>
     <div class="card border-0" v-for="movie in state" :key="movie.id">
       <div class="card-header border-0">
-        <h1>{{ movie.greekTitle }}</h1>
+        <h2 class="text-primary">{{ movie.greekTitle }}</h2>
         <div class="d-flex justify-content-between flex-wrap column-gap-3">
           <div>{{ movie.originalTitle }} </div>
           <div class="d-flex justify-content-end gap-3 align-items-center">
             <div>Σε {{ movie.cinemas.length }} σινεμά</div>
             <div> {{ formatDuration(movie.duration) }} </div>
             <div>{{ movie.year }} </div>
-            <div v-if="movie.imdbRating">{{ `${movie.imdbRating}/10` }} </div>
+            <div v-if="movie.imdbRating">
+              {{ movie.imdbRating === "None" ? 'Η βαθμολογία δεν βρέθηκε' : `${movie.imdbRating}/10` }}
+            </div>
             <div v-else class="rating-placeholder placeholder-glow"> <span class="placeholder rounded-3 col-12"></span>
             </div>
           </div>
@@ -52,12 +41,11 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import { useMoviesStore } from '@/stores/movies';
+import { onMounted, inject } from 'vue';
+// import { useMoviesStore } from '@/stores/movies';
 
-const moviesStore = useMoviesStore();
-const state = ref(moviesStore.MOVIES);
-const sortedBy = ref(1)
+// const moviesStore = useMoviesStore();
+const state = inject("state");
 
 const formatDuration = (minutes) => {
   const hours = Math.floor(minutes / 60);
@@ -73,7 +61,6 @@ const formatDuration = (minutes) => {
 };
 
 const sortByPopularity = () => {
-  sortedBy.value = 1;
   state.value = state.value.sort((a, b) => {
     if (a.cinemas.length !== b.cinemas.length) {
       return b.cinemas.length - a.cinemas.length;
@@ -91,51 +78,7 @@ const sortByPopularity = () => {
   });
 };
 
-const sortByRating = () => {
-  sortedBy.value = 2;
-  state.value = state.value.sort((a, b) => {
-    if (a.imdbRating && b.imdbRating) {
-      if (b.imdbRating !== a.imdbRating) {
-        return b.imdbRating - a.imdbRating;
-      }
-    }
-    if (!a.imdbRating && b.imdbRating) {
-      return 1;
-    }
-    if (a.imdbRating && !b.imdbRating) {
-      return -1;
-    }
-    return b.cinemas.length - a.cinemas.length;
-  });
-};
-
-const filterByToday = () => {
-  const date = new Date();
-  const today = date.toLocaleDateString('en-US', { weekday: 'long' });
-  state.value = state.value.filter((film) => {
-    return film.cinemas.some((cinema) => {
-      return cinema.cinemaSchedule[today] && cinema.cinemaSchedule[today].length > 0;
-    });
-  });
-};
-
-const resetDayFilter = () => {
-  state.value = moviesStore.MOVIES
-}
-
-const filterBySelectedDays = (days) => {
-  days = ['Wednesday', 'Monday']
-  state.value = state.value.filter((film) => {
-    return film.cinemas.some((cinema) => {
-      return days.some((day) => {
-        return cinema.cinemaSchedule[day] && cinema.cinemaSchedule[day].length > 0;
-      })
-    });
-  });
-};
-
 onMounted(() => {
-  state.value = moviesStore.MOVIES;
   sortByPopularity()
 });
 </script>
