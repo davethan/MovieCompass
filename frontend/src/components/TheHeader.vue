697e69a2d77374cdf90adf1ad499de5afd439f54
David Athanasios Desipris <ddesipris@improvit.gr>
2024-11-28 09:28:01 +0200

    Better styling @header and drawer

--- frontend/src/components/TheHeader.vue
+++ frontend/src/components/TheHeader.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="navbar sticky-top navbar-dark bg-dark px-3">
     <div class="d-flex gap-3 justify-content-between align-items-center">
-      <i v-if="route.name === 'home'" class="bi bi-list fs-3 text-secondary cursor-pointer" data-bs-toggle="offcanvas"
+      <i v-if="route.name === 'home'" class="bi bi-list fs-3 cursor-pointer" data-bs-toggle="offcanvas"
         data-bs-target="#homeViewDrawer" />
-      <i v-else-if="route.name === 'IndividualMovie'" class="bi bi-film fs-3 text-secondary cursor-pointer"
-        data-bs-toggle="offcanvas" data-bs-target="#individualMovieDrawer" />
+      <i v-else-if="route.name === 'IndividualMovie'" class="bi bi-film fs-3 cursor-pointer" data-bs-toggle="offcanvas"
+        data-bs-target="#individualMovieDrawer" />
       <i v-else class="bi bi-arrow-left fs-3 cursor-pointer" @click="router.back" />
-      <h4 class="m-0 cursor-pointer" @click="router.push({ name: 'home' })">Athens Cinemas</h4>
+      <h4 class="m-0 cursor-pointer text-secondary" @click="router.push({ name: 'home' })">Athens Cinemas</h4>
     </div>
     <div class="ml-auto">{{ state.length }}/{{ moviesStore.MOVIES.length }}</div>
   </div>
