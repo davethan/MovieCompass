5b65b7b301510c604923fdd4d963dc34667836e0
Danis Desipris <danis_desipris@hotmail.com>
2024-12-02 21:33:17 +0200

    better scrollToTop & hotness/diamond feature

--- frontend/src/shared/ScrollToTopButton.vue
+++ frontend/src/shared/ScrollToTopButton.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="fade">
-    <div v-if="isScrollToTopButtonShown" class="scrollToTopButton rounded-5 px-4 py-3 cursor-pointer"
+    <div v-if="isScrollToTopButtonShown" class="scrollToTopButton rounded-5 px-5 py-2 cursor-pointer"
       @click="scrollToTop">
       <i class="bi bi-arrow-up fs-5 cursor-pointer" />
     </div>
@@ -16,8 +16,8 @@ const isScrollToTopButtonShown = ref(false);
 
 const handleScroll = () => {
   const scrollTop = window.scrollY || document.documentElement.scrollTop;
-  if (lastScrollTop.value - scrollTop >= 50) {
-    if (scrollTop < 300) isScrollToTopButtonShown.value = false
+  if (lastScrollTop.value - scrollTop >= 5) {
+    if (scrollTop < 350) isScrollToTopButtonShown.value = false
     else isScrollToTopButtonShown.value = true;
   }
   else if (scrollTop > lastScrollTop.value) isScrollToTopButtonShown.value = false;
