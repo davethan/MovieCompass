8c20c8d62ec14853a42d12532b37adc68117c2a4
Danis Desipris <danis_desipris@hotmail.com>
2025-01-10 01:22:15 +0200

    Better drawer component

--- frontend/src/shared/Drawer.vue
+++ frontend/src/shared/Drawer.vue
@@ -1,23 +1,23 @@
 <template>
-  <div :class="['offcanvas', classDirection]" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" :id="id"
-    :aria-labelledby="id">
-    <div class="offcanvas-header">
-      <button type="button" class="mx-0 py-0 btn-close" aria-label="Close" @click="close"></button>
-      <div class="ms-auto offcanvas-title" :id="id">
-        <slot name="drawerHeader"></slot>
+  <transition :name="drawerTransition" @before-enter="handleBeforeEnter" @after-enter="handleAfterEnter">
+    <div v-if="isOpen" :class="['offcanvas show', classDirection]" data-bs-scroll="true" data-bs-backdrop="false"
+      tabindex="-1" :id="id" :aria-labelledby="id" ref="drawerElement">
+      <div class="offcanvas-header">
+        <button type="button" class="mx-0 py-0 btn-close" aria-label="Close" @click="close"></button>
+        <div class="ms-auto offcanvas-title" :id="id">
+          <slot name="drawerHeader"></slot>
+        </div>
+      </div>
+      <div class="offcanvas-body">
+        <slot name="drawerBody"></slot>
       </div>
     </div>
-    <div class="offcanvas-body">
-      <slot name="drawerBody"></slot>
-    </div>
-  </div>
+  </transition>
 </template>
 
 <script setup>
-import { watch, computed } from 'vue';
-import { useRoute } from 'vue-router';
+import { onBeforeUnmount, onMounted, watch, computed, ref } from 'vue';
 
-const route = useRoute();
 const emit = defineEmits(['update:modelValue']);
 
 const props = defineProps({
@@ -36,19 +36,53 @@ const props = defineProps({
 });
 
 const classDirection = computed(() => props.position === 'left' ? 'offcanvas-start' : 'offcanvas-end')
+const drawerTransition = computed(() => props.position === 'left' ? 'drawer-fade-left' : 'drawer-fade-right')
 
-watch(() => props.modelValue, (newValue) => {
-  const drawerElement = document.getElementById(props.id);
-  if (drawerElement) {
-    if (newValue) drawerElement.classList.add('show');
-    else drawerElement.classList.remove('show');
-  }
-});
+const isOpen = ref(false);
+const isOpening = ref(false);
+const drawerElement = ref(null);
 
-watch(() => route.path, (newPath, oldPath) => {
-  if (newPath === oldPath) return
-  close();
-});
-
+const handleClickOutside = (event) => {
+  if (drawerElement.value && !drawerElement.value.contains(event.target) && !isOpening.value) close();
+}
+const handleBeforeEnter = () => isOpening.value = true;
+const handleAfterEnter = () => isOpening.value = false;
 const close = () => emit('update:modelValue', false);
+
+watch(() => props.modelValue, (newValue) => isOpen.value = newValue);
+
+onMounted(() => {
+  document.addEventListener("click", handleClickOutside);
+})
+onBeforeUnmount(() => {
+  document.removeEventListener("click", handleClickOutside);
+})
 </script>
+
+<style scoped>
+.drawer-fade-left-leave-active,
+.drawer-fade-right-leave-active {
+  transition: translate 0.3s ease;
+}
+
+.drawer-fade-left-leave-to {
+  translate: -100%;
+}
+
+.drawer-fade-right-leave-to {
+  translate: 100%;
+}
+
+.drawer-fade-left-enter-active,
+.drawer-fade-right-enter-active {
+  transition: translate 0.3s ease;
+}
+
+.drawer-fade-left-enter-from {
+  translate: -100%;
+}
+
+.drawer-fade-right-enter-from {
+  translate: 100%;
+}
+</style>
