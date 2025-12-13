7c1136c9c749e46a64ee3374645e0ca28eb998b2
David Athanasios Desipris <ddesipris@improvit.gr>
2025-12-13 10:14:37 +0200

    Added trailer

--- frontend/src/components/IndividualMovie.vue
+++ frontend/src/components/IndividualMovie.vue
@@ -1,130 +1,140 @@
 <template>
-  <div>
-    <div class="card border-0 mb-3 col-lg-6 mx-auto">
-      <div class="card-header">
-
-        <!-- bigger than small -->
-        <div class="d-none d-sm-block">
-          <div class="d-flex justify-content-between gap-2">
-            <img :src="state.imageUrl" :alt="state.greekTitle" class="img-fluid">
-            <div class="d-flex flex-grow-1 flex-column">
-              <h2 class="text-primary m-0">{{ state.greekTitle }}</h2>
-              <div class="col-12 mb-2">{{ state.originalTitle }} </div>
-              <div class="row g-2">
-                <div class="col-12 d-flex justify-content-start flex-wrap gap-1 align-items-center">
-                  <div class="tag-outlined">Σε {{ state.cinemas.length }} σινεμά</div>
-                  <div class="tag-outlined"> {{ formatDuration(state.duration) }} </div>
-                  <div class="tag-outlined">{{ state.year }} </div>
-                  <div class="tag-outlined" v-if="state.imdbRating">
-                    {{ state.imdbRating === "None" ? '?/10' : `${state.imdbRating}/10` }}
-                  </div>
-                  <div v-else class="rating-placeholder placeholder-glow"> <span
-                      class="placeholder rounded-3 col-12"></span>
-                  </div>
-                  <div v-if="state.rated" class="tag-outlined">{{ state.rated }}</div>
-                </div>
-                <div class="col-12 d-flex justify-content-start flex-wrap gap-1 align-items-center">
-                  <div v-for="(tag, i) in state.tags" :key="i" class="tag-square">{{ tag }}</div>
-                </div>
-              </div>
-            </div>
-            <i class="bi bi-list fs-3 cursor-pointer" @click="openMovieDrawer" />
-          </div>
-        </div>
-
-        <!-- small -->
-        <div class="d-block d-sm-none">
-          <div class="row g-2 mb-2">
-            <img :src="state.imageUrl" :alt="state.greekTitle" class="col-3 img-fluid">
-            <div class="col-8 d-flex flex-column">
-              <h2 class="text-primary m-0">{{ state.greekTitle }}</h2>
-              <div class="col-12">{{ state.originalTitle }} </div>
-            </div>
-            <i class="col-1 bi bi-list fs-3 cursor-pointer" @click="openMovieDrawer" />
-          </div>
-          <div class="row g-2">
-            <div class="col-12 d-flex justify-content-start flex-wrap gap-1 align-items-center">
-              <div class="tag-outlined">Σε {{ state.cinemas.length }} σινεμά</div>
-              <div class="tag-outlined"> {{ formatDuration(state.duration) }} </div>
-              <div class="tag-outlined">{{ state.year }} </div>
-              <div class="tag-outlined" v-if="state.imdbRating">
-                {{ state.imdbRating === "None" ? '?/10' : `${state.imdbRating}/10` }}
-              </div>
-              <div v-else class="rating-placeholder placeholder-glow"> <span
-                  class="placeholder rounded-3 col-12"></span>
-              </div>
-              <div v-if="state.rated" class="tag-outlined">{{ state.rated }}</div>
-            </div>
-            <div class="col-12 d-flex justify-content-start flex-wrap gap-1 align-items-center">
-              <div v-for="(tag, i) in state.tags" :key="i" class="tag-square">{{ tag }}</div>
-            </div>
-          </div>
-        </div>
-
-      </div>
-      <div class="card-body">
-        <div class="d-flex gap-3">
-          <div>
-            <b>Σκηνοθεσία: </b><template v-for="(director, index) in state.directors">
-              {{ director }}<span :key="index" v-if="index < state.directors.length - 1">, </span>
-            </template>
-          </div>
-        </div>
-        <div class="my-4">
-          {{ state.summary }}
-        </div>
-        <div v-if="state.actors.length" class="mb-2">
-          <b>Παίζουν: </b> <template v-for="(actor, index) in state.actors">
-            {{ actor }}<span :key="index" v-if="index < state.actors.length - 1">, </span>
-          </template>
-        </div>
-        <div v-if="state.awards" class="mb-2">
-          <b>Βραβεία: </b><span> {{ state.awards }}</span>
-        </div>
-        <div class="d-flex justify-content-end align-items-end gap-2">
-          <a :href="athinoramaUrl" target="_blank" rel="noopener noreferrer">
-            <img src="@/assets/images/athinorama.jpg" alt="Athinorama" width="30" height="30" class="cursor-pointer" />
-          </a>
-          <a v-if="state.imdbLink" :href="state.imdbLink" target="_blank" rel="noopener noreferrer">
-            <img src="@/assets/images/imdb.png" alt="IMDb" width="30" height="30" class="cursor-pointer" />
-          </a>
-        </div>
-      </div>
-    </div>
-    <div class="d-flex flex-column justify-content-center flex-md-row gap-3 flex-md-wrap">
-      <expanding-circle-background v-for="(cinema, i) in filteredCinemas" :key="i"
-        cssClass="card border-0 cinema-item cursor-pointer" @clicked="goToCinemaPage(cinema)">
+  <div class="row g-2 mb-3">
+    <div class="col-lg-6">
+      <div class="card border-0">
         <div class="card-header">
-          <h2 class="text-secondary">{{ cinema.cinema }}</h2>
-          <div class="d-flex justify-content-between flex-wrap ">
-            <div>{{ cinema.cinemaLocation }}</div>
-            <div v-if="cinema.isOutdoor"><i class="bi bi-brightness-high-fill is-outdoor" /></div>
+
+          <!-- bigger than small -->
+          <div class="d-none d-sm-block">
+            <div class="d-flex justify-content-between gap-2">
+              <img :src="state.imageUrl" :alt="state.greekTitle" class="img-fluid">
+              <div class="d-flex flex-grow-1 flex-column">
+                <h2 class="text-primary m-0">{{ state.greekTitle }}</h2>
+                <div class="col-12 mb-2">{{ state.originalTitle }} </div>
+                <div class="row g-2">
+                  <div class="col-12 d-flex justify-content-start flex-wrap gap-1 align-items-center">
+                    <div class="tag-outlined">Σε {{ state.cinemas.length }} σινεμά</div>
+                    <div class="tag-outlined"> {{ formatDuration(state.duration) }} </div>
+                    <div class="tag-outlined">{{ state.year }} </div>
+                    <div class="tag-outlined" v-if="state.imdbRating">
+                      {{ state.imdbRating === "None" ? '?/10' : `${state.imdbRating}/10` }}
+                    </div>
+                    <div v-else class="rating-placeholder placeholder-glow"> <span
+                        class="placeholder rounded-3 col-12"></span>
+                    </div>
+                    <div v-if="state.rated" class="tag-outlined">{{ state.rated }}</div>
+                  </div>
+                  <div class="col-12 d-flex justify-content-start flex-wrap gap-1 align-items-center">
+                    <div v-for="(tag, i) in state.tags" :key="i" class="tag-square">{{ tag }}</div>
+                  </div>
+                </div>
+              </div>
+              <i class="bi bi-list fs-3 cursor-pointer" @click="openMovieDrawer" />
+            </div>
           </div>
+
+          <!-- small -->
+          <div class="d-block d-sm-none">
+            <div class="row g-2 mb-2">
+              <img :src="state.imageUrl" :alt="state.greekTitle" class="col-3 img-fluid">
+              <div class="col-8 d-flex flex-column">
+                <h2 class="text-primary m-0">{{ state.greekTitle }}</h2>
+                <div class="col-12">{{ state.originalTitle }} </div>
+              </div>
+              <i class="col-1 bi bi-list fs-3 cursor-pointer" @click="openMovieDrawer" />
+            </div>
+            <div class="row g-2">
+              <div class="col-12 d-flex justify-content-start flex-wrap gap-1 align-items-center">
+                <div class="tag-outlined">Σε {{ state.cinemas.length }} σινεμά</div>
+                <div class="tag-outlined"> {{ formatDuration(state.duration) }} </div>
+                <div class="tag-outlined">{{ state.year }} </div>
+                <div class="tag-outlined" v-if="state.imdbRating">
+                  {{ state.imdbRating === "None" ? '?/10' : `${state.imdbRating}/10` }}
+                </div>
+                <div v-else class="rating-placeholder placeholder-glow"> <span
+                    class="placeholder rounded-3 col-12"></span>
+                </div>
+                <div v-if="state.rated" class="tag-outlined">{{ state.rated }}</div>
+              </div>
+              <div class="col-12 d-flex justify-content-start flex-wrap gap-1 align-items-center">
+                <div v-for="(tag, i) in state.tags" :key="i" class="tag-square">{{ tag }}</div>
+              </div>
+            </div>
+          </div>
+
         </div>
         <div class="card-body">
-          <template v-if="typeof cinema.cinemaSchedule === 'string' || cinema.cinemaSchedule instanceof String">
-            {{ cinema.cinemaSchedule }}
-          </template>
-          <div v-else>
-            <template v-for="[dayName, hours] in Object.entries(cinema.cinemaSchedule)">
-              <div v-if="hours.length" class="row my-2" :key="dayName">
-                <div class="col-5 text-truncate">
-                  {{ mapDayName(dayName) }}
-                </div>
-                <div class="col-7 d-flex flex-wrap column-gap-2">
-                  <span v-for="(hour, i) in hours" :key="i">
-                    {{ hour }}
-                  </span>
-                </div>
-              </div>
+          <div class="d-flex gap-3">
+            <div>
+              <b>Σκηνοθεσία: </b><template v-for="(director, index) in state.directors">
+                {{ director }}<span :key="index" v-if="index < state.directors.length - 1">, </span>
+              </template>
+            </div>
+          </div>
+          <div class="my-4">
+            {{ state.summary }}
+          </div>
+          <div v-if="state.actors.length" class="mb-2">
+            <b>Παίζουν: </b> <template v-for="(actor, index) in state.actors">
+              {{ actor }}<span :key="index" v-if="index < state.actors.length - 1">, </span>
             </template>
           </div>
+          <div v-if="state.awards" class="mb-2">
+            <b>Βραβεία: </b><span> {{ state.awards }}</span>
+          </div>
+          <div class="d-flex justify-content-end align-items-end gap-2">
+            <a :href="athinoramaUrl" target="_blank" rel="noopener noreferrer">
+              <img src="@/assets/images/athinorama.jpg" alt="Athinorama" width="30" height="30"
+                class="cursor-pointer" />
+            </a>
+            <a v-if="state.imdbLink" :href="state.imdbLink" target="_blank" rel="noopener noreferrer">
+              <img src="@/assets/images/imdb.png" alt="IMDb" width="30" height="30" class="cursor-pointer" />
+            </a>
+          </div>
         </div>
-      </expanding-circle-background>
+      </div>
     </div>
-    <individual-movie-drawer v-model="isMovieDrawerOpen" @filter-changed="filterCinemas" :state="state" />
-    <scroll-to-top-button />
+    <div class="col-lg-6">
+      <div class="card film-item-no-hover">
+        <div class="card-body" style="min-height: 350px">
+          <iframe :src="state.trailer" style="width: 100%; height:100%" />
+        </div>
+      </div>
+    </div>
   </div>
+  <div class="d-flex flex-column justify-content-center flex-md-row gap-3 flex-md-wrap">
+    <expanding-circle-background v-for="(cinema, i) in filteredCinemas" :key="i"
+      cssClass="card border-0 cinema-item cursor-pointer" @clicked="goToCinemaPage(cinema)">
+      <div class="card-header">
+        <h2 class="text-secondary">{{ cinema.cinema }}</h2>
+        <div class="d-flex justify-content-between flex-wrap ">
+          <div>{{ cinema.cinemaLocation }}</div>
+          <div v-if="cinema.isOutdoor"><i class="bi bi-brightness-high-fill is-outdoor" /></div>
+        </div>
+      </div>
+      <div class="card-body">
+        <template v-if="typeof cinema.cinemaSchedule === 'string' || cinema.cinemaSchedule instanceof String">
+          {{ cinema.cinemaSchedule }}
+        </template>
+        <div v-else>
+          <template v-for="[dayName, hours] in Object.entries(cinema.cinemaSchedule)">
+            <div v-if="hours.length" class="row my-2" :key="dayName">
+              <div class="col-5 text-truncate">
+                {{ mapDayName(dayName) }}
+              </div>
+              <div class="col-7 d-flex flex-wrap column-gap-2">
+                <span v-for="(hour, i) in hours" :key="i">
+                  {{ hour }}
+                </span>
+              </div>
+            </div>
+          </template>
+        </div>
+      </div>
+    </expanding-circle-background>
+  </div>
+  <individual-movie-drawer v-model="isMovieDrawerOpen" @filter-changed="filterCinemas" :state="state" />
+  <scroll-to-top-button />
 </template>
 
 <script setup>
