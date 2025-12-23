7f59bc4fb3282f5cace6ac6958218b3c8e8d6906
Danis Desipris <danis_desipris@hotmail.com>
2025-12-24 00:51:17 +0200

    Prevented scroll-bubling

--- frontend/src/components/CardTransition.vue
+++ frontend/src/components/CardTransition.vue
@@ -5,19 +5,21 @@
 </template>
 
 <style>
-.list-move,
-.list-enter-active,
-.list-leave-active {
-  transition: all 0.5s ease;
-}
+@media (min-width: 992px) {
 
-.list-enter-from,
-.list-leave-to {
-  opacity: 0;
-  transform: translateX(30px);
-}
+  .list-move,
+  .list-enter-active,
+  .list-leave-active {
+    transition: all 0.5s ease;
+  }
 
-.list-leave-active {
-  position: absolute;
+  .list-enter-from,
+  .list-leave-to {
+    opacity: 0;
+  }
+
+  .list-leave-active {
+    position: absolute;
+  }
 }
 </style>
