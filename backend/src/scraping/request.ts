15d4d6e6bf85d94ab16e96f3362dea18b35cd818
Danis Desipris <danis_desipris@hotmail.com>
2026-03-28 19:21:16 +0200

    Tried to bypass imdb's security with better headers + npm updates

--- backend/src/scraping/request.ts
+++ backend/src/scraping/request.ts
@@ -1,11 +1,23 @@
 import axios from 'axios';
+import { wrapper } from 'axios-cookiejar-support';
+import { CookieJar } from 'tough-cookie';
 
-export const request = axios.create({
+const jar = new CookieJar();
+
+export const request = wrapper(axios.create({
+    jar,
+    withCredentials: true,
     headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'Accept-Language': 'en-US,en;q=0.9',
         'Accept-Encoding': 'gzip, deflate, br',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-        'Connection': 'keep-alive'
+        'Referer': 'https://www.google.com/',
+        'DNT': '1', // Do Not Track
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'cross-site',
+        'Sec-Fetch-User': '?1',
     }
-});
+}));
